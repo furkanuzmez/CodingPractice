676c4a87871aaa840ea7a0645a0956078c0e9b40
Furkan <codedeveloper2648@gmail.com>
2024-12-16 21:37:06 +0100

    feat(frequencyCounter): maxCharacter

--- leetCode/frequencyCounter.js
+++ leetCode/frequencyCounter.js
@@ -1,14 +1,24 @@
 function frequencyCounter(arr) {
     const frequency = {};
+    let maxNumber = 0;
+    let maxKey = '';
 
     for (let item of arr) {
         frequency[item] = (frequency[item] || 0) + 1;
     }
-
-    return frequency;
+    
+    for(const key in frequency) {
+        // maxNumber = frequency[key];
+        // maxKey = key
+        if(maxNumber < frequency[key]) {
+           maxNumber = frequency[key];
+           maxKey = key
+        }
+    }
+    return maxKey;
 }
 
 // Example usage:
-const array = ['a', 'b', 'a', 'c', 'b', 'a', 'd'];
+const array = ['a', 'b', 'b', 'c', 'b', 'a', 'b'];
 const result = frequencyCounter(array);
 console.log(result); // Output: { a: 3, b: 2, c: 1, d: 1 }
